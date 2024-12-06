d3ecf36a79495ee5dd821310ee87b62a0ff96536
nlinhdang <nlinhdang94@gmail.com>
2024-12-06 16:53:18 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Label from './components/Label';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Label />
+    </div>
+  );
+}
+
+export default App;
